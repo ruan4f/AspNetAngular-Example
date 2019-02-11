26dfad0565feaa5c29790016b617dc3f1b760fd6
Ruan <ruan4f@gmail.com>
2019-02-10 23:31:34 -0200

    Created the dataRepository and was added a new package

--- AspNetAngular/Startup.cs
+++ AspNetAngular/Startup.cs
@@ -4,6 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using AspNetAngular.Models;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using AspNetAngular.Repositories;
+
 namespace AspNetAngular
 {
   public class Startup
@@ -22,6 +27,7 @@ namespace AspNetAngular
             services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SQLConnection")));
             services.AddCors();
             services.AddAutoMapper();
+            services.AddScoped(typeof(IDataRepository < > ), typeof(DataRepository < > ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
