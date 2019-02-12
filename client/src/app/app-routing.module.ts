83c75174c22d8f84f3184752c07ebab164d16845
Ruan <ruan4f@gmail.com>
2019-02-11 22:56:38 -0200

    Added new routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SupplierComponent } from './supplier/supplier.component';
+import { SupplierDetailComponent } from './supplier-detail/supplier-detail.component';
+import { SupplierAddComponent } from './supplier-add/supplier-add.component';
+import { SupplierEditComponent } from './supplier-edit/supplier-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'supplier',
+    component: SupplierComponent,
+    data: { title: 'List of Suppliers' }
+  },
+  {
+    path: 'supplier-details/:id',
+    component: SupplierDetailComponent,
+    data: { title: 'Supplier Details' }
+  },
+  {
+    path: 'supplier-add',
+    component: SupplierAddComponent,
+    data: { title: 'Add Supplier' }
+  },
+  {
+    path: 'supplier-edit/:id',
+    component: SupplierEditComponent,
+    data: { title: 'Edit Supplier' }
+  },
+  { path: '',
+    redirectTo: '/supplier',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
