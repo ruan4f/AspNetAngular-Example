f8a7d8277042aef13a067c0e472c01bdf26bdb03
Ruan <ruan4f@gmail.com>
2019-02-11 23:17:23 -0200

    Changes in supplier component

--- client/src/app/supplier/supplier.component.ts
+++ client/src/app/supplier/supplier.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { ApiService } from '../api.service';
 
+import { Supplier } from '../supplier';
+
 @Component({
   selector: 'app-supplier',
   templateUrl: './supplier.component.html',
@@ -9,9 +11,22 @@ import { ApiService } from '../api.service';
 })
 export class SupplierComponent implements OnInit {
 
+  displayedColumns: string[] = ['supplierId', 'companyName', 'contactName'];
+  data: Supplier[] = [];
+  isLoadingResults = true;
+
   constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.api.getSuppliers()
+      .subscribe(res => {
+        this.data = res;
+        console.log(this.data);
+        this.isLoadingResults = false;
+      }, err => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
 
 }
